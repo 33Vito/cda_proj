6c02aaa6933ae20c7d3287a3d53df088886e60b8
33Vito <tony.yanjun@yahoo.com>
2021-04-25 12:36:59 +1000

    Add news timeline for AU

--- gravity_covid_TL/R_data_prepocessing.R
+++ gravity_covid_TL/R_data_prepocessing.R
@@ -1,7 +1,22 @@
 source("R_functions.R")
 
-#-----------------------------Geocodes for places of interest in SYD------------------------------------------
-SYD_hospitals <- readxl::read_excel("./data/SYD_geocodes/Hospitals/Latest data (points).xlsx", skip = 2)
+# -------------SYD confirmed cases by postcode----------------
+confirmed_cases <- read_csv("data/confirmed_cases_table1_location.csv")
+
+# -------------SYD shp file data------------------------------
+SYD_POA <- readRDS("data/SYD_POA.rds")
+SYD_LGA <- readRDS("data/SYD_LGA.rds")
+SYD_SA2 <- readRDS("data/SYD_SA2.rds")
+
+#--------------AU covid timeline data ---------
+
+AU_timeline <- readxl::read_excel("./data/AU_covid19_timeline.xlsx", 
+                                  sheet = 1) %>% 
+  mutate(date = as.Date(date))
+
+#--------------Geocodes for places of interest in SYD---------
+SYD_hospitals <- readxl::read_excel(
+  "./data/SYD_geocodes/Hospitals/Latest data (points).xlsx", skip = 2)
 SYD_hospitals$bed.approx <- fct_recode(SYD_hospitals$Beds, 
                                    "25" = "<50", 
                                    "75" = "20-99", 
@@ -14,21 +29,24 @@ SYD_hospitals$bed.approx <- fct_recode(SYD_hospitals$Beds,
 SYD_hospitals$bed.approx[is.na(SYD_hospitals$bed.approx)] <- 25
 # gp <- read_csv("./data/SYD_geocodes/Hospitals/GP.csv")
 
-
-SYD_pschools <- readxl::read_excel("./data/SYD_geocodes/Primary Schools/Primary Schools.xlsx") %>% 
+SYD_pschools <- readxl::read_excel(
+  "./data/SYD_geocodes/Primary Schools/Primary Schools.xlsx") %>% 
   filter(!duplicated(poiname)) %>% 
   filter(Long >= 150, Long <= 152)
 
-SYD_sschools <- readxl::read_excel("./data/SYD_geocodes/Secondary Schools/Secondary Schools.xlsx") %>% 
+SYD_sschools <- readxl::read_excel(
+  "./data/SYD_geocodes/Secondary Schools/Secondary Schools.xlsx") %>% 
   filter(!duplicated(poiname)) %>% 
   filter(Long >= 150, Long <= 152)
 
-SYD_supermarkets <- read_csv("./data/SYD_geocodes/Shopping centres/Supermarket & grocery stores.csv") %>% 
+SYD_supermarkets <- read_csv(
+  "./data/SYD_geocodes/Shopping centres/Supermarket & grocery stores.csv") %>% 
   filter(!duplicated(address)) %>% 
   filter(lon >= 150, lon <= 152) %>% 
   na.omit()
 
-SYD_shops <- read_csv("./data/SYD_geocodes/Shopping centres/Shopping centres.csv") %>% 
+SYD_shops <- read_csv(
+  "./data/SYD_geocodes/Shopping centres/Shopping centres.csv") %>% 
   filter(lon >= 150, lon <= 152) %>% 
   na.omit()
 
@@ -43,3 +61,5 @@ SYD_lightrails <- read_csv("./data/SYD_geocodes/Trains/LightRails.csv") %>%
 
 SYD_metro <- read_csv("./data/SYD_geocodes/Trains/Metro.csv") %>% 
   filter(lon >= 150, lon <= 152)
+
+
