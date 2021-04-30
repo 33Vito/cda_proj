f686394c5df358684499c0bc107b054523c279b3
Fred Yu <zhikang.yu@yahoo.com>
2021-05-01 02:05:40 +1000

    Update on analysis result

--- clustering_fy/Development/clustering.R
+++ clustering_fy/Development/clustering.R
@@ -28,25 +28,29 @@ SSC_2016_AUST_CLEAN = SSC_2016_AUST %>%
   mutate(SSC_NAME_2016 = str_trim(str_remove(SSC_NAME_2016, b_pattern))) %>%
   select(SSC_CODE_2016, SSC_NAME_2016, STATE_NAME_2016) %>%
   distinct() %>%
+  dplyr::distinct(ss, .keep_all = TRUE)  %>%
   arrange(SSC_CODE_2016)
 
 # Income distress by suburb
 income_2016 = read_csv("clustering_fy/Data/census_data/2016_Household_income.csv")[, c(1, 4:5)]
 colnames(income_2016) = c("ss", "low_income", "high_income")
 income_2016 = income_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Income by suburb
 income2_2016 = read_csv("clustering_fy/Data/census_data/2016_Median_weekly_incomes.csv")[, c(1:4)]
 colnames(income2_2016) = c("ss", "p_income", "f_income", "h_income")
 income2_2016 = income2_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Mortgage by suburb
 mortgage_2016 = read_csv("clustering_fy/Data/census_data/2016_Mortgage_monthly_repayments.csv")[, c(1, 2, 7)]
 colnames(mortgage_2016) = c("ss", "repay", "repay_distress")
 mortgage_2016 = mortgage_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Language by suburb
 language_top_responses_2016 = read_csv("clustering_fy/Data/census_data/2016_Language_top_responses5.csv")
@@ -56,29 +60,35 @@ language_2016 = language_top_responses_2016 %>%
   mutate(chinese_a = replace_na(Mandarin, 0) + replace_na(Cantonese, 0)) %>%
   mutate(chinese_p = replace_na(`Mandarin (%)`, 0)+replace_na(`Cantonese (%)`, 0)) %>%
   mutate(all_p = round(chinese_a*100/chinese_p, 0)) %>%
-  filter(chinese_p >= 5.2) %>%
-  select(ss, chinese_p)
+  select(ss, chinese_p) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Tenure by suburb
 tenure_2016 = read_csv("clustering_fy/Data/census_data/2016_Tenure.csv") %>%
   select(c(1,7:9))
 colnames(tenure_2016) = c("ss", "owner", "mortgage", "rent")
 tenure_2016 = tenure_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Rent by suburb
 rent_2016 = read_csv("clustering_fy/Data/census_data/2016_Rent_weekly_payments.csv") %>%
   select(c(1, 2, 7))
 colnames(rent_2016) = c("ss", "rental", "rent_distress")
 rent_2016 = rent_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Dwelling structure by suburb
 dw_2016 = read_csv("clustering_fy/Data/census_data/2016_Occupied_private_dwellings.csv") %>%
   select(c(1, 6:8))
 colnames(dw_2016) = c("ss", "house", "semi", "unit")
 dw_2016 = dw_2016 %>%
-  mutate(ss = str_trim(str_remove(ss, b_pattern)))
+  mutate(ss = str_trim(str_remove(ss, b_pattern))) %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
+
+dw_2016 %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # SSC to POA
 ssc_poa = read_csv("clustering_fy/Data/POA to SSC mapping.csv") %>%
@@ -91,6 +101,11 @@ ssc = read_csv("clustering_fy/Data/SSC_final.csv") %>%
   filter(!is.na(POA_CODE_2016)) %>%
   select(ss, ssc, POA_CODE_2016)
 
+# COVID input
+covid = read_csv("clustering_fy/Data/confirmed_cases_table4_location_likely_source.csv") %>%
+  filter(substr(likely_source_of_infection, 1, 5) == 'Local') %>%
+  select(notification_date, postcode, lga_name19)
+
 # Suburb data together
 all_table = income_2016 %>%
   inner_join(income2_2016, by = 'ss') %>%
@@ -99,7 +114,8 @@ all_table = income_2016 %>%
   inner_join(rent_2016, by = "ss") %>%
   inner_join(mortgage_2016, by = "ss") %>%
   inner_join(dw_2016, by = "ss") %>%
-  inner_join(ssc, by = "ss")
+  inner_join(ssc, by = "ss") %>%
+  dplyr::distinct(ss, .keep_all = TRUE)
 
 # Suburbs with cluster
 all_table_cluster = bind_cols(all_table, all_table %>%
@@ -108,59 +124,19 @@ all_table_cluster = bind_cols(all_table, all_table %>%
   helper_clustering() %>%
   select(cluster))
 
-# COVID input
-covid = read_csv("clustering_fy/Data/confirmed_cases_table4_location_likely_source.csv") %>%
-  filter(substr(likely_source_of_infection, 1, 5) == 'Local') %>%
-  select(notification_date, postcode, lga_name19)
+all_table_h_cluster = bind_cols(all_table %>% select(ss, ssc, POA_CODE_2016), all_table %>%
+                                select(-c("ss", "ssc", "POA_CODE_2016")) %>%
+                                helper_pc_convert() %>%
+                                helper_h_clustering() %>%
+                                select(cluster))
 
-# Link post code
-covid_cluster = covid %>%
-  left_join(all_table_cluster, by = c("postcode"="POA_CODE_2016")) %>%
-  select(notification_date, postcode, lga_name19, cluster) %>% 
-  filter(!is.na(cluster))
-
-# Give a key to each covid case
-covid_cluster =  covid_cluster %>%
-  bind_cols(data.frame(case=1:nrow(covid_cluster)))
-
-# Identify how many near future cases can be identified from previous clusters
-covid_predicted = covid_cluster %>%
-  left_join(covid_cluster, by = "cluster") %>%
-  filter(notification_date.y - notification_date.x < 3, notification_date.y - notification_date.x > 0) %>%
-  select(c(6, 7, 8, 9, 4)) %>%
-  dplyr::distinct() %>%
-  arrange(case.y)
-
-# Initial effectiveness
-effectiveness = nrow(covid_predicted) / max(covid_cluster$case)
-
-effectiveness
-
-all_table %>%
-  group_by(ss) %>%
-  tally() %>%
-  arrange(desc(n))
-
-all_table1 = all_table %>%
-  select(-ss) %>%
-  as.data.frame()
-
-rownames(all_table1) = all_table$ss
-
-pc = principal(scale(all_table1), nfactors = ncol(all_table1), rotate='varimax')
-pc
-plot(pc$values[1:4], type="b", main="Scree Plot")
+all_table_cluster = bind_cols(all_table %>% select(ss, ssc, POA_CODE_2016), all_table %>%
+                                  select(-c("ss", "ssc", "POA_CODE_2016")) %>%
+                                  helper_pc_convert() %>%
+                                  helper_clustering() %>%
+                                  select(cluster))
 
 
-k = kmeans(all_table1, 6)
-k$cluster
 
-all_table2 = cbind(all_table1, as.factor(k$cluster)) %>%
-  mutate(ss = rownames(all_table1))
-
-ggplot(as.data.frame(pc$scores), aes(x=RC1, y=RC2, color=as.factor(all_table2$`as.factor(k$cluster)`))) + 
-  geom_point(size = 0.01) + theme_bw() + 
-  # geom_text(aes(label=rownames(pc$scores)),hjust=0, vjust=0, size = 3) +
-  ggtitle('First 2 prinipal components clustering w/ kmeans') + 
-  xlab("Principal Component 1") + ylab("Principal Component 2")
-
+helper_effective(all_table_h_cluster, lockdon_n = 14)
+helper_effective(all_table_cluster, lockdon_n = 14)
