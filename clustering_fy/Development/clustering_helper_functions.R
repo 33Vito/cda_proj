9de54eb6c1c0c75e0d5a7c99092cf884649dc68e
Fred Yu <zhikang.yu@yahoo.com>
2021-05-01 00:58:07 +1000

    Hierarchical clustering helper function added

--- clustering_fy/Development/clustering_helper_functions.R
+++ clustering_fy/Development/clustering_helper_functions.R
@@ -2,6 +2,7 @@
 # Convert original datasets' raw features to PC features
 helper_pc_convert = function(table, cutoff=0.9) {
   pc = principal(scale(table), nfactors = ncol(table), rotate='varimax')
+  # min n required for var explanability to reach cutoff point
   n_keep = min(which(cumsum(pc$values/sum(pc$values))>cutoff))
   
   all_table_pc = data.frame(pc$scores)[, 1:n_keep]
@@ -16,3 +17,14 @@ helper_clustering = function(table) {
   table$cluster = k$cluster
   return(table)
 }
+
+# Empirically determine n by setting an average of 8 suburbs per cluster
+# Hierarchical clustering
+helper_h_clustering = function(table) {
+  n = floor(nrow(table)/8)
+  hierar = hclust(dist(table))
+  fit = cutree(hierar, k = n)
+  table$cluster = fit
+  return(table)
+}
+
