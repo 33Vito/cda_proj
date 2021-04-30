b474359df64b9da61f1876a93086aa2d889fdad6
Fred Yu <zhikang.yu@yahoo.com>
2021-05-01 00:44:16 +1000

    Update helper function to include cutoff for pca

--- clustering_fy/Development/clustering_helper_functions.R
+++ clustering_fy/Development/clustering_helper_functions.R
@@ -1,9 +1,10 @@
 # Principle component transform
 # Convert original datasets' raw features to PC features
-helper_pc_convert = function(table) {
+helper_pc_convert = function(table, cutoff=0.9) {
   pc = principal(scale(table), nfactors = ncol(table), rotate='varimax')
+  n_keep = min(which(cumsum(pc$values/sum(pc$values))>cutoff))
   
-  all_table_pc = data.frame(pc$scores)
+  all_table_pc = data.frame(pc$scores)[, 1:n_keep]
   
   return(all_table_pc)
 }
